cfc997f74a6f66e066770ab3bc2f2b4011f94fc4
marcelo <almagro@comcast.net>
2013-09-24 11:28:33 -0700

    first run

---
+++ prototypeTango/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrototypeTango::Application.config.secret_token = '31694a2a329d753b584b1619edf0b126e1172faebb515d3a0583b272f969eaa7814adb52930128c01d1ccee2afa1726d690d3161fd8c0fc49066aedc12038d8f'
